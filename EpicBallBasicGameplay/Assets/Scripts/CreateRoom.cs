7af5daa6a0627c47b63c17132c12b110b9424208
Naani <snstheja@gmail.com>
2019-03-30 22:46:28 -0700

    PhotonImplementation

---
+++ EpicBallBasicGameplay/Assets/Scripts/CreateRoom.cs
@@ -0,0 +1,38 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateRoom : MonoBehaviour
+{
+    [SerializeField]
+    private Text _RoomName;
+    private Text _RoomText
+    {
+        get { return _RoomName; }
+    }
+   
+    public void OnClickCreateRoom()
+    {
+        if(PhotonNetwork.CreateRoom(_RoomText.text))
+        {
+            print("Create Room successfully sent: ");
+        }
+        else
+        {
+            print("create room failed");
+        }
+
+    }
+
+    private void OnPhotonCreateRoomFailed(Object[] codeAndMessage)
+    {
+        print("Create room failed" + codeAndMessage[1]);
+    }
+
+    private void OnCreatedRoom()
+    {
+        print("Room Created Successfully");
+    }
+}
---
+++ EpicBallBasicGameplay/Assets/Scripts/LobbyNetwork.cs
@@ -0,0 +1,28 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyNetwork : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Connecting to server");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    private void OnConnectedToMaster()
+    {
+        print("connected to master");
+        PhotonNetwork.NickName = PlayerNetwork.Instance.PlayerName;
+
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+    private void OnJoinedLobby()
+    {
+        print("Joined Lobby");
+    }
+
+  
+}
