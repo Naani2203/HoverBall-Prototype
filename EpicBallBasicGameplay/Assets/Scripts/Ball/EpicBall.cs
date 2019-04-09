c5e7c43f6ba40929a2fd0dccc2b869830a5d7e07
Naani <snstheja@gmail.com>
2019-04-09 10:26:04 -0700

    fixed passing bug

--- EpicBallBasicGameplay/Assets/Scripts/Ball/EpicBall.cs
+++ EpicBallBasicGameplay/Assets/Scripts/Ball/EpicBall.cs
@@ -20,13 +20,14 @@ public class EpicBall : MonoBehaviourPun
         if(collision.collider.CompareTag("Player") && collision.gameObject.GetComponent<ShootTheBall>()._HaveBall == false)
         {
             collision.gameObject.GetComponent<ShootTheBall>()._HaveBall = true;
-            if(PhotonNetwork.IsMasterClient)
+           
             photonView.RPC("RPC_DestroyBall", RpcTarget.All);
         }
     }
     [PunRPC]
     private void RPC_DestroyBall()
     {
-        Destroy(this.gameObject);
+        if (PhotonNetwork.IsMasterClient)
+            Destroy(this.gameObject);
     }
 }
