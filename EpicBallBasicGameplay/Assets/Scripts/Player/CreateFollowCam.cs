906be3ad167018bf5af0533fe6bdf922f70e4a6d
Naani <snstheja@gmail.com>
2019-04-15 20:51:13 -0700

    rgef

--- EpicBallBasicGameplay/Assets/Scripts/Player/CreateFollowCam.cs
+++ EpicBallBasicGameplay/Assets/Scripts/Player/CreateFollowCam.cs
@@ -7,22 +7,27 @@ public class CreateFollowCam : MonoBehaviour
     [SerializeField]
     private GameObject _FollowCamera;
     [SerializeField]
+    private GameObject _FollowLerp;
+    [SerializeField]
     private Transform _FollowPoint;
     [SerializeField]
     private Transform _LookAtPoint;
     private GameObject _CameraInScene;
+    private GameObject _FollowLerpInScene;
 
     private void Awake()
     {
         Instantiate(_FollowCamera, transform.position, Quaternion.identity);
+        Instantiate(_FollowLerp, transform.position, Quaternion.identity);
         _CameraInScene = GameObject.FindGameObjectWithTag("MainCamera");
+        _FollowLerpInScene = GameObject.FindGameObjectWithTag("FollowVector");
     }
 
     // Update is called once per frame
     void Update()
     {
-                           
 
-            _CameraInScene.GetComponent<CameraFollow>().FollowCameraPoint(_FollowPoint,_LookAtPoint);
+        _FollowLerpInScene.GetComponent<CameraFollow>().FollowLookAtPoint(_LookAtPoint);
+            _CameraInScene.GetComponent<CameraFollow>().FollowCameraPoint(_FollowPoint,_FollowLerpInScene.transform);
     }
 }
